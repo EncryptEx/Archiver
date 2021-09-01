d42bca2fdc3572c3ce59c42c70204077e4fdef85
EncryptEx <limusina10@gmail.com>
2021-09-01 15:11:31 +0200

    Add log function.
    Code cleanup

--- custom.py
+++ custom.py
@@ -5,17 +5,28 @@ import time
 hassettings = os.path.exists('savesettings.archiversettings')
 wantsToContinue=True
 
+def YesNoQuestion(question):
+	answ = input(question)
+	if(answ in ["Y","y","YES","Yes","yes","True","true","TRUE"]):
+		return True
+	else: 
+		return False
+
+def CheckYesNoValue(value):
+	if(value in ["Y","y","YES","Yes","yes","True","true","TRUE"]):
+		return True
+	else: 
+		return False
+
 if (not hassettings):
-	print("Looks like you have not settings saved yet.")
-	answ = input("Would you like to create them? (Y/N): ")
-	if (answ not in ["Y","y","N","n"]): 
-		print("Didn't undestand you. Please answer with Y (yes) or N (not)")
-		quit()
-	
-	if (answ in ["N","n"]): wantsToContinue=False
+	#no settings found
+	print("Looks like you have not created a settings file yet.")
+	wantsToContinue = YesNoQuestion("Would you like to create it? (Y/N): ")
+
 
 	if (wantsToContinue):
-		#create file
+
+		#create SETTINGS file
 		fh=open("savesettings.archiversettings","x+",encoding="UTF-8")
 		
 
@@ -28,39 +39,46 @@ if (not hassettings):
 		fh.write("path="+tpath)
 		mypath = tpath
 		#ask for extra files 
-		efiles = input("Do you want extra files to be classified as well? (Y/N): ")
-		if (efiles == "" or efiles == None or len(efiles) <= 0): 
-			print("[ERROR] Can't be blank.")
-			quit()
-		if(efiles in ["Y","y","YES","Yes","yes","True","true","TRUE"]):
-			wantsextrafiles = True
-			print("[SETTINGS] Enabled Extra Files Folder Creation")
+		wantsextrafiles = YesNoQuestion("Do you want extra files to be classified as well? (Y/N): ")
+
+		if wantsextrafiles:
+			print("[SETTINGS] Extra Files Folder Creation Enabled ")
 		else: 
-			wantsextrafiles = False
-			print("[SETTINGS] Disabled Extra Files Folder Creation")
-		fh.write("\nWantsExtraFiles="+efiles)
+			print("[SETTINGS] Extra Files Folder Creation Disabled ")
+		fh.write("\nWantsExtraFiles="+str(wantsextrafiles))
 
 		#ask more things
-
+		answlog = YesNoQuestion("Do you want to log all organized files? (Y/N): ")
+		fh.write("\nLog="+str(answlog))
+		wantsTolog = answlog
+		if(answlog):
+			print("[SETTINGS] Log Enabled")
+		else:
+			print("[SETTINGS] Log Disabled")
 
 if (hassettings):
+	#settings detected
 	print("[OK] Settings File Found")
 	#read settings
 	fh=open("savesettings.archiversettings","r+",encoding="UTF-8")
 	mypath = fh.readline().strip()[len("path="):]
 
-	wantsextrafiles = fh.readline().strip()[len("WantsExtraFiles="):]
-
-	if(wantsextrafiles in ["Y","y","YES","Yes","yes","True","true","TRUE"]):
-		wantsextrafiles = True
-		print("[SETTINGS] Enabled Extra Files Folder Creation")
+	wantsextrafiles = CheckYesNoValue(fh.readline().strip()[len("WantsExtraFiles="):])
+	
+	if(wantsextrafiles):
+		print("[SETTINGS] Extra Files Folder Creation Enabled ")
 	else: 
-		wantsextrafiles = False
-		print("[SETTINGS] Disabled Extra Files Folder Creation")
+		print("[SETTINGS] Extra Files Folder Creation Disabled ")
+
+	wantsTolog = CheckYesNoValue(fh.readline().strip()[len("Log="):])
+	if(wantsTolog):
+		print("[SETTINGS] Log Enabled ")
+	else: 
+		print("[SETTINGS] Log Disabled ")
 
 if(not wantsToContinue): 
-
-	mypath = input("Enter full path of folder:")
+	#don't want to save settings
+	mypath = input("Enter the full path of the folder you want to organize:")
 	if (mypath == "" or mypath == None or len(mypath) <= 0): 
 		print("[ERROR] Can't be blank.") 
 		quit()
@@ -71,12 +89,8 @@ existsmypath = os.path.exists(mypath)
 
 if(not existsmypath):
 	print("[ERROR] Configuration File Corrupted. Or data invalid")
-	answ = input("Would you like to delete it? (Y/N): ")
-	if (answ not in ["Y","y","N","n"]): 
-		print("Didn't undestand you. Please answer with Y (yes) or N (not)")
-		quit()
-	
-	if (answ in ["N","n"]): quit()
+	answ = YesNoQuestion("Would you like to delete it? (Y/N): ")
+	if(not answ): quit()
 
 	fh.close()
 	os.remove("savesettings.archiversettings")
@@ -106,107 +120,128 @@ for element in stuff:
 
 #core funct
 def createDir(temppath,nameDir):
-	globals()[exception] = False
+	
 	newpath = temppath+nameDir
 	if not os.path.exists(newpath):
 		os.makedirs(newpath)
+		if(wantsTolog):
+			print("[LOG] New Folder Created:",newpath)
 
 def moveFile(path):
 	os.replace(data[file][1],path)
+	if(wantsTolog):
+		print("[LOG] File Moved OLD PATH=",data[file][1],"\n"+17*" "+"NEW PATH=",path)
 #create folder
 #get extensions
-exception = True
 
 for file in data:
-	extension =os.path.splitext(file)[1]
-	if(file == "desktop.ini" or file.endswith(".ini") or file.lower().endswith(".DAT")): continue
-	if file.endswith(".exe") or file.endswith(".msi"):
+	exception = True
+	extension = os.path.splitext(file)[1]
+	if(file in ["desktop.ini", "custom.py", "oneclick.py", "savesettings.archiversettings"] or file.endswith(".ini") or file.lower().endswith(".dat")): continue
+	if (extension in [".exe", ".msi"]):
 		exeDir = "executables/"
 		createDir(mypath,exeDir)
 		moveFile(mypath+exeDir+file)
+		exception = False
 
 	if (extension in [".png", ".img", ".jpg", ".jpeg", ".HEIC", ".gif", ".svg", ".webp"]):
 		imgDir = "images/"
 		createDir(mypath,imgDir)
 		moveFile(mypath+imgDir+file)
+		exception = False
 		
 	if (extension in [".mp4", ".mov"]):
 		videoDir = "videos/"
 		createDir(mypath,videoDir)
 		moveFile(mypath+videoDir+file)
+		exception = False
 
 	if (extension in [".wav", ".mp3", ".ogg", ".aac", ".m4a"]):
 		audioDir = "audios/"
 		createDir(mypath,audioDir)
 		moveFile(mypath+audioDir+file)
+		exception = False
 
 	if (extension in [".stl", ".obj"]):
 		threedDir = "3dFiles/"
 		createDir(mypath,threedDir)
 		moveFile(mypath+threedDir+file)
+		exception = False
 
 	if (extension in [".rar", ".zip", ".7z", ".tar", ".rar5"]):
 		zipDir = "compressed/"
 		createDir(mypath,zipDir)
 		moveFile(mypath+zipDir+file)
+		exception = False
 
 	if (extension in [".txt", ".md"]):
 		txtDir = "notepad/"
 		createDir(mypath,txtDir)
 		moveFile(mypath+txtDir+file)
+		exception = False
 
 	if (extension in [".jar", ".schematic", ".schem"]):
 		mcDir = "minecraft/"
 		createDir(mypath,mcDir)
 		moveFile(mypath+mcDir+file)
+		exception = False
 
 	if (extension in [".pdf", ".doc", ".docx", ".ppt", ".pptx", ".xls", ".csv"]):
 		docDir = "documents/"
 		createDir(mypath,docDir)
 		moveFile(mypath+docDir+file)
+		exception = False
 
 	if (extension in [".skp"]):
 		sketchup = "sketchup/"
 		createDir(mypath,sketchup)
 		moveFile(mypath+sketchup+file)
+		exception = False
 
 	if (extension in [".html"]):
 		htmlDir = "htmls/"
 		createDir(mypath,htmlDir)
 		moveFile(mypath+htmlDir+file)
+		exception = False
 
 	if (extension in [".psd"]):
 		psdDir = "psd/"
 		createDir(mypath,psdDir)
 		moveFile(mypath+psdDir+file)
+		exception = False
 
 	if (extension in [".torrent"]):
 		torrentDir = "torrents/"
 		createDir(mypath,torrentDir)
 		moveFile(mypath+torrentDir+file)
+		exception = False
 
 	if (extension in [".bsdesign"]):
 		bs4design = "bs4design/"
 		createDir(mypath,bs4design)
 		moveFile(mypath+bs4design+file)
+		exception = False
 
 	if (extension in [".log"]):
 		logDir = "logs/"
 		createDir(mypath,logDir)
 		moveFile(mypath+logDir+file)
+		exception = False
 
 	if (extension in [".log"]):
 		logDir = "logs/"
 		createDir(mypath,logDir)
 		moveFile(mypath+logDir+file)
+		exception = False
 
 	if (extension in [".ovpn"]):
 		ovpnDir = "ovpn/"
 		createDir(mypath,ovpnDir)
 		moveFile(mypath+ovpnDir+file)
+		exception = False
 
 	if (exception):
-		valuePoint = file.find(".")
+		valuePoint = file.rfind(".")
 		if(valuePoint):
 			if (wantsextrafiles):
 				extrafile = file[valuePoint+1:]+"/"
